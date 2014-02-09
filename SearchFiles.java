969e6ac5e20b3a6bcc7d3a6b6a2cc7c92110fef1
Karthik Chandrasekar <karthidec1@gmail.com>
2014-02-09 12:48:02 -0700

    Modularize the code

--- SearchFiles.java
+++ SearchFiles.java
@@ -2,7 +2,6 @@ package edu.asu.irs13;
 import org.apache.lucene.index.*;
 import java.util.*;
 import org.apache.lucene.store.*;
-import org.apache.lucene.document.*;
 import java.io.File;
 import java.util.Scanner;
 class ValueComparator implements Comparator<String> {
@@ -23,108 +22,150 @@ class ValueComparator implements Comparator<String> {
 }
 
 public class SearchFiles {
-	public static void main(String[] args) throws Exception
-	{
-		
-		IndexReader r = IndexReader.open(FSDirectory.open(new File("index")));
-		int totalDocs = r.maxDoc();
-		System.out.println("The number of documents in this index is: " + r.maxDoc());
+    HashMap<Integer, Integer> twoNorm = new HashMap<Integer, Integer>();
 
-		int i = 0;
-		TermEnum t = r.terms();
+    public void getTwoNorm(IndexReader r) throws Exception
+    {
+        
+        TermEnum t = r.terms();
+        long startTime = System.currentTimeMillis();
+        int freq;
+        while(t.next())
+        {   
+            Term te = new Term("contents", t.term().text());
+            TermDocs td = r.termDocs(te);
+            while(td.next())
+            {
+                freq = 0;
+                if (twoNorm.containsKey(td.doc()))
+                {
+                    freq = twoNorm.get(td.doc());
+                }
+                freq += td.freq() * td.freq();
+                twoNorm.put(td.doc(), freq);
+            }
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("Time take for twoNorm compute is "+ (double)(endTime - startTime)/1000);
+    }
+    
+    
+    public void showResults(Map<String, Double> relMap)
+    {
+        long startTime = System.currentTimeMillis();
+        
+        //Override the comparator to sort the relMap by value. 
+        ValueComparator bvc =  new ValueComparator(relMap);
+        TreeMap<String,Double> sortedMap = new TreeMap<String,Double>(bvc);
+        sortedMap.putAll(relMap);
+        
+        //Print top ten high relevant elements
+        int loopVar = 0;
 
-		HashMap<Integer, Integer> twoNorm = new HashMap<Integer, Integer>();
-		
-		while(t.next())
-		{	
-			Term te = new Term("contents", t.term().text());
-			TermDocs td = r.termDocs(te);
-			int freq;
-			while(td.next())
-			{
-				freq = 0;
-				if (twoNorm.containsKey(td.doc()))
-				{
-					freq = twoNorm.get(td.doc());
-				}
-				freq += td.freq() * td.freq();
-				twoNorm.put(td.doc(), freq);
-			}
-		}
-		
-		
-		Scanner sc = new Scanner(System.in);
-		String str = "";
-		System.out.print("query> ");
-		HashMap<String, Double> relMapTfIdf = new HashMap<String, Double>();
-		HashMap<String, Double> relMapTf = new HashMap<String, Double>();
-		int queryLen;
-		double relTfIdf;
-		double relTf;
-		double Idf;
-		while(!(str = sc.nextLine()).equals("quit"))
-		{
-			String[] terms = str.split("\\s+");
-			queryLen = terms.length;
-			for(String word : terms)
-			{
-				Term term = new Term("contents", word);
-				TermDocs tdocs = r.termDocs(term);
-				while(tdocs.next())
-				{
-					relTf = 0;
-					relTfIdf = 0;
-					Idf = 0;
-					
-					if(relMapTfIdf.containsKey(tdocs.doc()))
-					{
-						relTfIdf = relMapTfIdf.get(tdocs.doc());
-					}
-					if(relMapTf.containsKey(tdocs.doc()))
-					{
-						relTf = relMapTf.get(tdocs.doc());
-					}
-					Idf = (double)(totalDocs/(double)r.docFreq(term));
-					relTfIdf += (double)((tdocs.freq() * Idf )/(Math.sqrt(queryLen) * Math.sqrt(twoNorm.get(tdocs.doc()))));
-					relMapTfIdf.put(Integer.toString(tdocs.doc()), relTfIdf);
-					relTf += (double)(tdocs.freq())/(Math.sqrt(queryLen) * Math.sqrt(twoNorm.get(tdocs.doc())));
-					relMapTf.put(Integer.toString(tdocs.doc()), relTf);
-					String d_url = r.document(tdocs.doc()).getFieldable("path").stringValue().replace("%%", "/");
-					
-				}
-			}
-			
-			//Override the comparator to sort the relMap by value. 
-			ValueComparator bvc =  new ValueComparator(relMapTfIdf);
-	        TreeMap<String,Double> sortedMapTfIdf = new TreeMap<String,Double>(bvc);
-	        sortedMapTfIdf.putAll(relMapTfIdf);
+        for(Map.Entry<String, Double> pair : sortedMap.entrySet())
+        {
+            loopVar++;
+            if(loopVar >9)
+                break;
+            System.out.println(pair.getKey());
+        }
+        loopVar = 0;
+        long endTime = System.currentTimeMillis();
+        System.out.println("Time taken for sorting stuffs compute is "+ (double)(endTime - startTime)/1000);
+    }
+    
+    
+    public void orderUsingTf(String str, IndexReader r, SearchFiles sObj, Map<String, Double> relMapTf) throws Exception
+    {
+        String[] terms = str.split("\\s+");
+        int queryLen = terms.length;
+        double relTf;
+        
+        long startTime = System.currentTimeMillis();
 
-	        ValueComparator bvcTf = new ValueComparator(relMapTf);
-	        TreeMap<String,Double> sortedMapTf = new TreeMap<String,Double>(bvcTf);
-	        sortedMapTf.putAll(relMapTf);
+        for(String word : terms)
+        {
+            Term term = new Term("contents", word);
+            TermDocs tdocs = r.termDocs(term);
+            while(tdocs.next())
+            {
+                relTf = 0;                  
+            
+                if(relMapTf.containsKey(tdocs.doc()))
+                {
+                    relTf = relMapTf.get(tdocs.doc());
+                }       
+                relTf += (double)(tdocs.freq())/(Math.sqrt(queryLen) * Math.sqrt(sObj.twoNorm.get(tdocs.doc())));
+                relMapTf.put(Integer.toString(tdocs.doc()), relTf);
+            }
+        
+        }
+        System.out.println("Results of Tf");
+        sObj.showResults(relMapTf);
+        long endTime = System.currentTimeMillis();
+        System.out.println("Ordering based on Tf results -Time Taken "+ (double)(endTime - startTime)/1000);
 
-	        //Print top ten high relevant elements
-	        int loopVar =0;
+    }
+    
+    public void orderUsingTfIdf(String str, IndexReader r, SearchFiles sObj, Map<String, Double> relMapTfIdf) throws Exception
+    {
+        String[] terms = str.split("\\s+");
+        int queryLen = terms.length;
+        double relTfIdf;
+        long startTime = System.currentTimeMillis();
+        double Idf;
+        int totalDocs = r.maxDoc();
 
-	        System.out.println("Sorted using Tf");
-	        for(Map.Entry<String, Double> pair : sortedMapTf.entrySet())
-	        {
-	        	loopVar++;
-	        	if(loopVar >9)
-	        		break;
-	        	System.out.println(pair.getKey());
-	        }
+        for(String word : terms)
+        {
+            Term term = new Term("contents", word);
+            TermDocs tdocs = r.termDocs(term);
+            while(tdocs.next())
+            {
+                relTfIdf = 0;                   
+                if(relMapTfIdf.containsKey(tdocs.doc()))
+                {
+                    relTfIdf = relMapTfIdf.get(tdocs.doc());
+                }
+                
+                Idf = (double)(totalDocs/(double)r.docFreq(term));
+                relTfIdf += (double)((tdocs.freq() * Idf )/(Math.sqrt(queryLen) * Math.sqrt(sObj.twoNorm.get(tdocs.doc()))));
+                relMapTfIdf.put(Integer.toString(tdocs.doc()), relTfIdf);       
+            }
+        }
+        System.out.println("Result of TfIdf");
+        sObj.showResults(relMapTfIdf);
+        long endTime = System.currentTimeMillis();
+        System.out.println("Ordering based on TfIdf results -Time Taken "+ (double)(endTime - startTime)/1000);
+    }
+    public static void main(String[] args) throws Exception
+    {
+        SearchFiles sObj = new SearchFiles();
+        IndexReader r = IndexReader.open(FSDirectory.open(new File("index")));
+        System.out.println("The number of documents in this index is: " + r.maxDoc());
+        
+        //Compute two norm for all the documents in the corpus
+        sObj.getTwoNorm(r);
+        Scanner sc = new Scanner(System.in);
+        String str = "";
+        System.out.print("query> ");
 
-	        loopVar = 0;
-	        System.out.println("Sorted using Tf/Idf");
-	        for(Map.Entry<String, Double> pair : sortedMapTfIdf.entrySet())
-	        {
-	        	loopVar++;
-	        	if(loopVar >9)
-	        		break;
-	        	System.out.println(pair.getKey());
-	        }
-			System.out.print("query> ");
-		}
-	}
+        while(!(str = sc.nextLine()).equals("quit"))
+        {   
+            long startTime = System.currentTimeMillis();
+            
+            HashMap<String, Double> relMapTf = new HashMap<String, Double>();
+            sObj.orderUsingTf(str, r, sObj, relMapTf);
+                    
+            HashMap<String, Double> relMapTfIdf = new HashMap<String, Double>();        
+            sObj.orderUsingTfIdf(str, r, sObj, relMapTfIdf);
+            
+            long endTime = System.currentTimeMillis();
+            
+            System.out.println("Time taken to get results "+ (double)(endTime - startTime)/1000);
+            System.out.print("query> ");
+        }
+        sc.close();
+    }
+    
 }
